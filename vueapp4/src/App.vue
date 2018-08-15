6605779cdfd0dac6e99c00f8ae80717495cf0a56
dongdong11111111 <1269648866@qq.com>
2018-08-15 23:11:47 +0800

    vueapp例子 音乐

---
+++ vueapp4/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+      <CommonHeader :selectMenu="selectMenu"></CommonHeader>
+      <router-view class="content"/>
+      <CommonFooter @changeTitle="changeTitle" :menuList="menuList"></CommonFooter>
+  </div>
+</template>
+
+<script>
+  //把组件引进来
+  import CommonHeader from '@/components/CommonHeader.vue';
+  import CommonFooter from '@/components/CommonFooter.vue';
+  export default{
+    components:{
+        CommonHeader,
+        CommonFooter
+    },
+      methods:{
+        changeTitle(menu){
+            this.selectMenu=menu;
+        }
+      },
+
+
+
+      data(){
+          return {
+              menuList: [
+                  {
+                      title: '电影',
+                      bgColor: '#e54847',
+                      name: 'movie',
+                      path: '/Movie'
+                  },{
+                      title: '音乐',
+                      bgColor: '#009688',
+                      name: 'music',
+                      path: '/Music'
+                  },{
+                      title: '书籍',
+                      bgColor: '#795548',
+                      name: 'book',
+                      path: '/Book'
+                  },{
+                      title: '图片',
+                      bgColor: '#3f51b5',
+                      name: 'photo',
+                      path: '/Photo'
+                  }
+              ],
+              selectMenu: {} // 当前选中的菜单
+          }
+      }
+
+}
+</script>
+
+<style lang="scss">
+
+.content{
+  margin: 1rem 0;
+}
+</style>
