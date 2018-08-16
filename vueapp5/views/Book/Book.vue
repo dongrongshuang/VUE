105ab3db64cf826c4d03a345bfb8f7af624facba
dongdong11111111 <1269648866@qq.com>
2018-08-16 20:28:21 +0800

    vue移动端开发网站 音乐歌词部分做完

---
+++ vueapp5/views/Book/Book.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>book</h1>
+        <span>bdafhduefbui</span>
+    </div>
+
+
+</template>
+
+
+<script>
+    export default {
+        created () {
+            this.$emit("switchTab", "book");
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vueapp5/views/Photo/Photo.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>photo</h1>
+</template>
+
+
+<script>
+    export default {
+        created () {
+            this.$emit("switchTab", "photo");
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
